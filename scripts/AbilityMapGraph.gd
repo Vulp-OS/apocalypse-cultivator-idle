cfd3740275210940393c255f4a902988fc53d73c
Ethan Rockwood <ethan@rockwood.dev>
2023-11-09 17:20:15 -0700

    Change from flat txt to sqlite for dao data

--- scripts/AbilityMapGraph.gd
+++ scripts/AbilityMapGraph.gd
@@ -1,50 +1,47 @@
 extends GraphEdit
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#get_zoom_hbox().visible = false
-	instantiate_dao_map()
+	instantiate_dao_from_db()
 	arrange_nodes()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
-func _on_connection_request(from_node, from_port, to_node, to_port):
-	# Don't connect to input that is already connected
-	push_warning(from_node)
-	
-	for con in get_connection_list():
-		if con.to == to_node and con.to_port == to_port:
-			return
-	
-	connect_node(from_node, from_port, to_node, to_port)
-
-func instantiate_dao_map():
-	var file = FileAccess.open("res://assets/daoConnections.txt", FileAccess.READ)
-	
-	while not file.eof_reached():
-		var line = file.get_line()
-		if line == "":
-			continue
-		else:
-			var dao = line.get_slice(",",0)
-			var parent = line.get_slice(",",1)
-			if dao != "" and not get_node_or_null(dao):
-				add_dao(dao)
-			if parent != "" and not get_node_or_null(parent):
-				add_dao(parent)
+func instantiate_dao_from_db():
+	var db = SQLite.new()
+	db.path = "res://assets/dao.sqlite"
+	db.open_db()
+	db.query("SELECT dao.name, dao.tier, parent FROM dao LEFT JOIN parents on dao.name=parents.name;")
+	var len = len(db.query_result)
+	for i in len:
+		var foundational = false
+		var dao = str(db.query_result[i]["name"])
+		var parent = str(db.query_result[i]["parent"])
+		var tier = db.query_result[i]["tier"]
+		if tier == 1:
+			foundational = true
+		
+		if not get_node_or_null(dao):
+			add_dao(dao, foundational)
+		if parent != "<null>" and not get_node_or_null(parent):
+			add_dao(parent)
 			
-			connect_node(dao, 0, parent, 0)
+		connect_node(dao, 0, parent, 0)
+		
+	db.close_db()
 
-func add_dao(dao):
+func add_dao(dao, foundational = false):
 	var newDao = GraphNode.new()
-	print(dao)
 	newDao.name = dao
 	newDao.title = dao
 	newDao.add_child(Control.new())
-	newDao.set_slot(0, true, 0, Color.WHITE, true, 0, Color.WHITE)
+	if foundational:
+		newDao.set_slot(0, false, 0, Color.WHITE, true, 0, Color.WHITE)
+	else:
+		newDao.set_slot(0, true, 0, Color.WHITE, true, 0, Color.WHITE)
 	newDao.connect("node_selected", _dao_selected)
 	newDao.connect("node_deselected", _dao_deselected)
 	
@@ -54,10 +51,13 @@ func _dao_selected():
 	var children = get_children()
 	for child in children:
 		if child.selected:
-			child.set_slot(0, true, 0, Color.YELLOW, true, 0, Color.YELLOW)
+			var left_enabled = child.is_slot_enabled_left(0)
+			
+			child.set_slot(0, left_enabled, 0, Color.GREEN, true, 0, Color.GREEN)
 			
 func _dao_deselected():
 	var children = get_children()
 	for child in children:
-		if child.get_connection_input_color(0) == Color.YELLOW:
-			child.set_slot(0, true, 0, Color.WHITE, true, 0, Color.WHITE)
+		if child.get_connection_output_color(0) == Color.GREEN:
+			var left_enabled = child.is_slot_enabled_left(0)
+			child.set_slot(0, left_enabled, 0, Color.WHITE, true, 0, Color.WHITE)
